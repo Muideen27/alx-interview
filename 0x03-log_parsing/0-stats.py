3141cc6760bb98ef282f1c86e0b84405222c11da
Muideen27 <ilorimuideen0000@gmail.com>
2023-08-16 12:00:21 +0000

    Log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,29 +1,42 @@
-total = 0  # Variable to keep track of total file size
-counter = 0  # Counter to keep track of the number of lines processed
-sc_dict = {'200': 0, '301': 0, '400': 0, '401': 0,
-           '403': 0, '404': 0, '405': 0, '500': 0}  # Dictionary to count status codes
+#!/usr/bin/python3
+''' module for log parsing '''
 
-def print_data(total):
-    ''' Function to print statistics for the input '''
-    print('File size: {}'.format(total))
-    for key, value in sorted(sc_dict.items()):
-        if value != 0:
-            print('{}: {}'.format(key, value))
+import sys
+
+# Initialize metrics
+total_file_size = 0
+status_code_counts = {}
 
 try:
-    for line in sys.stdin:  # Read each line from input
-        rline = line.split(" ")  # Split line into parts
-        if len(rline) > 4:  # Check if line has enough parts
-            code = rline[-2]  # Get the status code
-            if code in sc_dict.keys():
-                sc_dict[code] += 1  # Count the status code
-            filesize = int(rline[-1])  # Get the file size
-            total += filesize  # Add to total file size
-            counter += 1  # Increment the counter
-        if counter == 10:  # If 10 lines have been processed
-            counter = 0  # Reset the counter
-            print_data(total)  # Print statistics
-except Exception as ex:
-    pass
-finally:
-    print_data(total)  # Print final statistics
+    line_count = 0
+    for line in sys.stdin:
+        line_count += 1
+        
+        # Split the line into components
+        parts = line.strip().split()
+        if len(parts) != 7:
+            continue  # Skip lines with incorrect format
+        
+        ip_address, _, _, _, status_code, file_size = parts
+        
+        # Check if status code is a valid integer
+        if not status_code.isdigit():
+            continue
+        
+        # Update metrics
+        total_file_size += int(file_size)
+        status_code_counts[int(status_code)] = status_code_counts.get(int(status_code), 0) + 1
+        
+        # Print statistics every 10 lines
+        if line_count % 10 == 0:
+            print("Total file size:", total_file_size)
+            for code in sorted(status_code_counts.keys()):
+                print(f"{code}: {status_code_counts[code]}")
+            print()
+            
+except KeyboardInterrupt:
+    print("\nKeyboard interruption detected. Printing statistics:")
+    print("Total file size:", total_file_size)
+    for code in sorted(status_code_counts.keys()):
+        print(f"{code}: {status_code_counts[code]}")
+
